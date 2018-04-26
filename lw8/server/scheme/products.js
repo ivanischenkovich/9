905de8eb6354b5f28a98e05f4622ce9168561484
gstaizer <graywg3l@gmail.com>
2018-04-26 15:44:50 +0300

    Подключение валидатора

--- lw8/server/scheme/products.js
+++ lw8/server/scheme/products.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const idvalidator = require('mongoose-id-validator');
 
 const ProductSchema = new Schema({
   name: {
     type: String,
+    validate: /\S+/,
+    unique: [true],
+    required: [true]
   }
 });
+
+ProductSchema.plugin(idvalidator);
 
 const Product = mongoose.model('product', ProductSchema);
 module.exports = Product;
--- lw8/server/scheme/reviews.js
+++ lw8/server/scheme/reviews.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const idvalidator = require('mongoose-id-validator');
 
 const ReviewSchema = new Schema({
   productId: {
     type: String,
-  },
-
+	validate: /\S+/,
+    required: [true],
+    required: [true]
+  }
 });
+
+ReviewSchema.plugin(idvalidator);
 
 const Review = mongoose.model('review', ReviewSchema);
 
