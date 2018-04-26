84881d98a48af97670da7be163df8f28cf711b31
gstaizer <graywg3l@gmail.com>
2018-04-26 15:29:07 +0300

    Основные роуты

--- lw8/server/routes/routes.js
+++ lw8/server/routes/routes.js
@@ -9,4 +9,45 @@ const ERROR500 = {
   error: '500 Internal Server Error'
 };
 
+router.get('/products', function(req, res, next) {
+  Product.find({}).then(function(products) {
+    res.send(products);
+  }).catch(function(next) {
+    res.send({error: '500'});
+  });
+});
+
+router.get('/products/id', function(req, res, next) {
+  Product.findOne({_id: req.params.id}).then(function(product) {
+    res.send(product);
+  }).catch(function(next) {
+    res.send({error: '404'});
+  });
+});
+
+router.get('/reviews', function(req, res, next) {
+  Review.find({}).then(function(reviews) {
+    res.send(reviews);
+  }).catch(function(next) {
+    res.send({error: '500'});
+  });
+});
+
+router.get('/reviews/id', function(req, res, next) {
+  Review.findOne({_id: req.params.id}).then(function(review) {
+    res.send(review);
+  }).catch(function(next) {
+    res.send({error: '404'});
+  });
+});
+
+
+router.get('/products/id/reviews', function(req, res, next) {
+  Review.find({productId: req.params.id}).then(function(reviews) {
+    res.send(reviews);
+  }).catch(function(next) {
+    res.send({error: '404'});
+  });
+});
+
 module.exports = router;
