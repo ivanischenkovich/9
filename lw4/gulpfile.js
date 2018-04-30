7717afcd144bbb334a4556e6aa30756d3ff42f36
gstaizer <graywg3l@gmail.com>
2018-04-30 14:35:23 +0300

    fix

--- lw4/gulpfile.js
+++ lw4/gulpfile.js
@@ -26,6 +26,8 @@ gulp.task('minify-css', function () {
 gulp.task('html-build', function () {
   gulp.src('src/*.html')
   .pipe(gulp.dest('build/'))
+  gulp.src('src/js/jquery-3.2.1.min.js')
+  .pipe(gulp.dest('build/js'))
   .on('end', function () {
     gulp.src('build/index.html')
     .pipe(inject(gulp.src('./build/js/registration.min.js', {read: false}), {relative: true}))
@@ -33,33 +35,30 @@ gulp.task('html-build', function () {
     .pipe(gulp.dest('build/'));
   })
 });
-
+  
 gulp.task('dev', function () {
   browserSync.init({
 	server: {
-	  baseDir: "./src"
+	  baseDir: "./build"
 	}
   });
+  gulp.watch("./src/*").on('change', update);
   gulp.watch("./src/*").on('change', browserSync.reload);
 });
 
 gulp.task('build', function () {
-  function update () {
-    gulpSequence(
-	  "minify-js",
-	  "minify-css",
+  update ();
+  gulp.watch("./src/*").on('change', update);
+});
+
+function update () {
+  gulpSequence(
+    "minify-js",
+	"minify-css",
 	  "html-build",
 	  function () {
 	    gulp.src('src/css/style.css').pipe(gulp.dest('build/css'));
 		gulp.src('src/js/registration.js').pipe(gulp.dest('build/js'));
-      }
-    )
-  }
-  browserSync.init({
-    server: {
-      baseDir: "./build"
     }
-  });
-  update ();
-  gulp.watch("./src/*").on('change', update);
-});
+  )
+}
